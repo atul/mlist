5754a227767237bd52a477f349da211e98a506ef
Atul Patel <atul@atul-linux122.(none)>
2011-03-15 03:54:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mlist::Application.config.secret_token = '6590952669703a13953051cee8af64b0773e3997416ab0ef47d1b7b429de8863f8e26ff3c85278d0374b098b8366131e34421bef71583c574590d63b9be8dc44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mlist::Application.config.session_store :cookie_store, :key => '_mlist_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mlist::Application.config.session_store :active_record_store
